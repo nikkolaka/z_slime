5259f05e5905218a40a4a742e7ec35ea086123a2
Nicholas Zhuk <nicholaszhuk@gmail.com>
2023-06-20 14:29:46 -0700

    terrain generation working

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,20 @@ use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
 const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 450;
-const CELLS_WIDTH: i16 = 100;
-const CELLS_HEIGHT: i16 = 100;
+const SCREEN_HEIGHT: u32 = 900;
+const CELLS_WIDTH: i16 = 300;
+const CELLS_HEIGHT: i16 = 300;
 const CELLS_X: usize = 100;
-const CELLS_Y: usize = 100;
-const SCALE: f32 = 3.0;
-const DENSITY: f32 = 0.5;
+const CELLS_Y: usize = 200;
+const SCALE: f32 = 2.0;
 
 /// Representation of the application state. In this example, a box will bounce around the screen.
 struct World {
+    density: f32,
     width: i16,
     height: i16,  
     scale: f32, 
-    tiles: Vec<Vec<(f32, f32, f32)>>,
+    tiles: Vec<Vec<Cell>>,
 }
 
 fn main() -> Result<(), Error> {
@@ -67,6 +67,27 @@ fn main() -> Result<(), Error> {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
+            if input.key_pressed(VirtualKeyCode::Left) {
+                world.update_seed();
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Up) {
+                world.density += 0.02;
+                world.update_seed();
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Down) {
+                world.density -= 0.02;
+                world.update_seed();
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Right) {
+                world.update_map_iteration();
+                return;
+            }
 
             // Resize the window
             if let Some(size) = input.window_resized() {
@@ -76,7 +97,7 @@ fn main() -> Result<(), Error> {
                     return;
                 }
             }
-            world.update();
+            
             window.request_redraw();
 
             // Update internal state and request a redraw
@@ -96,25 +117,59 @@ impl World {
     /// Create a new `World` instance that can draw a moving box.
     fn new() -> Self {
         Self {
+            density: 0.4,
             width: CELLS_WIDTH,
             height: CELLS_HEIGHT,
             scale: SCALE,   
-            tiles: vec![vec![(0.0, 0.0, 0.0); CELLS_HEIGHT.try_into().unwrap()]; CELLS_WIDTH.try_into().unwrap()],
+            tiles: vec![vec![Cell::Empty; CELLS_HEIGHT.try_into().unwrap()]; CELLS_WIDTH.try_into().unwrap()],
         }
     }
 
     /// Update the `World` internal state; bounce the box around the screen.
-    fn update(&mut self) {
+    fn update_seed(&mut self) {
         for x in 0..self.width{
             for y in 0..self.height{
-                let wall = if rand::random::<f32>() > DENSITY {1.0}else{0.0};
+                let tile = if rand::random::<f32>() > self.density {Cell::Empty}else{Cell::Wall};
 
-                self.tiles[y as usize][x as usize] = (wall, wall, wall);
+                self.tiles[y as usize][x as usize] = tile;
                 
             }
         }
+    }
 
-    
+    fn update_map_iteration(&mut self) {
+        let mut new_tiles = vec![vec![Cell::Empty; CELLS_HEIGHT.try_into().unwrap()]; CELLS_WIDTH.try_into().unwrap()];
+        for x in 0..self.width{
+            for y in 0..self.height{
+                let mut neighbors = 0;
+                for i in -1..2{
+                    for j in -1..2{
+                        if i == 0 && j == 0{
+                            continue;
+                        }
+                        let x = x + i;
+                        let y = y + j;
+                        if x < 0 || x >= self.width || y < 0 || y >= self.height{
+                            continue;
+                        }
+                        if self.tiles[y as usize][x as usize] == Cell::Empty{
+                            neighbors += 1;
+                        }
+                    }
+                }
+                let mut tile = self.tiles[y as usize][x as usize];
+                if neighbors > 4{
+                    // empty
+                    tile = Cell::Empty;
+                } else if neighbors < 4{
+                    // wall
+                    tile = Cell::Wall;
+                    
+                }
+                new_tiles[y as usize][x as usize] = tile;
+            }
+        }
+        self.tiles = new_tiles;
     }
 
     /// Draw the `World` state to the frame buffer.
@@ -133,20 +188,30 @@ impl World {
            let rgba = if inside_cells{
                 let row: usize = ((y - CELLS_Y as i16) as f32 / self.scale) as usize % CELLS_HEIGHT as usize;
                 let col: usize = ((x - CELLS_X as i16) as f32 / self.scale) as usize % CELLS_WIDTH as usize;
-                let (r, g, b) = self.tiles[row][col];
-                if r == 1.0{
+                let tile = self.tiles[row][col];
+                if tile == Cell::Empty{
+                    //white empty cell
                     [0xff, 0xff, 0xff, 0xff]
                 } else {
+                    //black wall cell
                     [0x00, 0x00, 0x00, 0xff]
                 }
                 
                 
             } else{
-                [0x00, 0xff, 0xff, 0xff]
+                //white background
+                [0xff, 0xff, 0xff, 0xff]
             };
 
             pixel.copy_from_slice(&rgba);
 
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Cell{
+    Empty,
+    Wall,
+    Life (i16, i16, i16)
 }
